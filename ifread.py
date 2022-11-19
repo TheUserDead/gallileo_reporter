019cf920fc92161500101428d81a72df03beebfb
UserD <theuserdead@gmail.com>
2022-11-19 16:42:32 +0300

    new parser

--- ifread.py
+++ ifread.py
@@ -2,28 +2,24 @@ import serial, time, json, sys
 from datetime import datetime
 # here we need connect parsefile and get parseschema from device firmware JSON
 #rework with file selector!!!
-#hw 11 - v1.8.5 lite
-#hw 17 - v2.3.5
-#hw 19 - v5.0.0
-#hw 130 - 7.0 lite
+
 def init_comm():
   print("<i> Init com port")
   serialcmd = "<CMD REGIME 192837465>"
   ser.write(serialcmd.encode())
+  time.sleep(1)
   s = ser.read(25)        # read up to ten bytes (timeout)
   print(s)
   serialcmd = "XYZ ttt"
   ser.write(serialcmd.encode())
-  s = ser.read(25)
+  time.sleep(1)
+  s = ser.read_until(b'\x00')
   print(s)
   serialcmd = "XYZ 0"
   ser.write(serialcmd.encode())
-  s = ser.read(25)
+  time.sleep(1)
+  s = ser.read_until(b'\x00')
   print(s)
-  #serialcmd = "status"
-  #ser.write(serialcmd.encode())
-  #s = ser.read(150)
-  #print(s)
   print("<i> Com init complete")
   
 def req_ver():
@@ -45,24 +41,28 @@ try:
   serialcmd = input("ENter page: ")
   x = "IF {}".format(serialcmd)
   ser.write(x.encode())
-  s = ser.read(8) #bytes size here 
+  time.sleep(1)
+  print(ser.in_waiting)
   if ser.in_waiting == 0: #check response. if not port need to be initialized with device software
     init_comm()
-    s = ser.read(8) #bytes size here
+    s = ser.read(7) #bytes size here
     x = "IF {}".format(serialcmd)
     print(x)
     ser.write(x.encode())
-    s = ser.read(8) #bytes size here 
-  sz = s[3:-1] #cut substring with size
-  ssz = int(sz.decode('utf-8')) #convert to right data type
-  #print("size = {}".format(ssz)) #report this to console
-  s = ser.read(ssz) # read data stream from serial counted by received size
-  print(s.hex())
+    s = ser.read(7) #bytes size here
+
+  s = ser.read_until(b'\x20') #verify start packet
+  print(s)
+  if s.decode() == "IF ":
+    s = ser.read_until(b'\x20') #bytes size heree
+    ssz = int(s.decode('utf-8')) #convert to right data type
+    print("size = {}".format(ssz)) #report this to console
+    s = ser.read(ssz) # read data stream from serial counted by received size
+    print(s.hex())
 except serial.serialutil.SerialException:
-  print("Com port not found! Check connection!")
+  print("<!> Com port not found! Check connection!")
   sys.exit()
-#print(s.hex()) #report what we got
-#r = ['{:02x}'.format(s) for s in list(s)] #format received data in 2 bytes hex in array
+
 fileselector = input("ENter file: ")
 try: 
   with open('profiles/{}.json'.format(fileselector)) as file:
@@ -94,41 +94,58 @@ bytebuff = bytearray()
 for n in range(22):
   datax.append(s[pointerjson:][:datasizes[n]].hex())
   pointerjson = pointerjson + datasizes[n]
-
+print("------UNCONVERTED DATA!----------")
+for n in range(22): 
+  out = "dataname: {} data: {}".format(datanames[n], datax[n])
+  print(out)
 
 #define some vars before read convert type and pre-handle data using data type
 for n in range(22):
   if datatypes[n] == "uint" or datatypes[n] == "int" or datatypes[n] == "sat":
     if datasizes[n] > 1: #no need byte swapping if data size just one byte
-      #print("uint - long")
       bytebuff = bytearray.fromhex(datax[n])
       bytebuff.reverse() #-------------
       if datatypes[n] == "uint":
         datax[n] = int.from_bytes(bytebuff, "big", signed=False)
-        #print("  unsigned")
       if datatypes[n] == "int":
         datax[n] = int.from_bytes(bytebuff, "big", signed=True)
-        #print("  signed")
-      #print(datax[n])
     if datasizes[n] == 1:
-      #print("uint - short")
       datax[n] = int(datax[n], 16)
   if datatypes[n] == "string":
-    if datanames[n] != "USER":
+    if datasizes[n] < 20:
       bytebuff = bytearray.fromhex(datax[n])
       datax[n] = bytebuff.decode()
-  if datatypes[n] == "dt":
-    #print("dt")
-    datax[n] = int(datax[n], 16)
+      
+  if datatypes[n] == "dt": ## this datatype in latest version has little-endian against big-endian in old devices. 
+     #time where we live, starts from 1 digit. That's how i verify byteorder in time representation
+    #buff = int(datax[n], 16) # test convert
+    #print(buff)
+    bytebuff = bytearray.fromhex(datax[n])
+    #bytebuff.reverse()
+    buff = int.from_bytes(bytebuff, "big", signed=False)
+    magic = "1"
+    if str(buff)[0] == magic:
+      print("data from big")
+    if str(buff)[0] != magic:
+      print("data from little")
+    #buff = int.from_bytes(bytebuff, "big", signed=False)
+    
+    sys.exit()
 #############################
 ser.close()
-
+print("-----------DARSED DATA!------------")
 for n in range(22): 
   out = "dataname: {} data: {}".format(datanames[n], datax[n])
   print(out)
-print(datetime.utcfromtimestamp(datax[5]).strftime('%Y-%m-%d %H:%M:%S'))
+#print(datetime.utcfromtimestamp(datax[5]).strftime('%Y-%m-%d %H:%M:%S'))
+
+
 #AVAILABLE DATATYPES
 # uint\int - integer little endian possible the same just byte size 8x 16x 32x 64x etc.
 # string - just string big endian
 # dt - datetime unixtime
 # lat\lon
+#hw 11 - v1.8.5 lite
+#hw 17 - v2.3.5
+#hw 19 - v5.0.0
+#hw 130 - 7.0 lite
