df98e030746769d7d89203d7a8d5f4d422873a53
UserD <theuserdead@gmail.com>
2022-11-28 14:02:57 +0300

    finally batch woking

--- gpsconn.py
+++ gpsconn.py
@@ -1,24 +1,27 @@
 import serial, time, json, sys, glob, logging
 from datetime import datetime
 
-logging.basicConfig(level=logging.DEBUG, filename="gps_connector.log",filemode="w")
+logging.basicConfig(level=logging.DEBUG, filename="g_conn.log",filemode="a")
 
 def check_comm():
-  serialcmd = "status"
+  print("<i> Check comm")
+  serialcmd = "XYZ 0"
   ser.write(serialcmd.encode())
   time.sleep(0.5)
-  print(ser.in_waiting)
+  print("Waiting data: {}".format(ser.in_waiting))
   if ser.in_waiting > 0:
-    ser.flush()
-    return True
+    s = ser.read(ser.in_waiting)
+    if s == (b'XYZ OK\x00'):
+      ser.flush()
+      time.sleep(0.5)
+      return True
   else:
-    ser.flush()
     return False
 
 def init_comm():
+  print("<i> Init com port")
   if ser.in_waiting > 0:
     ser.flush()
-  print("<i> Init com port")
   serialcmd = "<CMD REGIME 192837465>"
   ser.write(serialcmd.encode())
   time.sleep(1)
@@ -48,6 +51,7 @@ def init_comm():
     sys.exit()
   
 def req_ver():
+  print("<i> Request version")
   hhw = comm_interface("hardversion").split("=")
   ssv = comm_interface("status").split("=")
   print(hhw)
@@ -62,8 +66,8 @@ def req_ver():
   
 #need define answer type. Answer can be just ascii text or IF0xxx where x - size of data
 def comm_interface(commandstr):
-  if ser.in_waiting > 0:
-    ser.flush()
+  print("<i> Command Interface")
+  ser.flush()
   ser.write(commandstr.encode())
   time.sleep(0.1)
   if ser.in_waiting == 0:
@@ -81,6 +85,7 @@ def comm_interface(commandstr):
 
 
 def file_attach():
+  print("<i> File attach")
   try: 
     filelist = glob.glob("profiles/*.json") #get all json files on directory
     files = [word[9:-5] for word in filelist] # magically get only filenames 
@@ -98,9 +103,7 @@ def file_attach():
       dataj = file.read()
     parsedj = json.loads(dataj)
     itemsize = len(parsedj["archive"]["item"]) # size of tags in json schema
-    datasizes = [];
-    datanames = [];
-    datatypes = [];
+
 
 #Get data from schema, sizes\names\types
     for n in range(22):
@@ -117,53 +120,57 @@ def file_attach():
 
 #connect to serial adapter
 def serialconn():
+  print("<i> Serr connn")
   try:
     global ser 
     ser = serial.Serial('/dev/ttyACM0', 19200, bytesize=8, parity='N', timeout=3, rtscts=0, xonxoff=0)
-    if check_comm() == False:
+    comreq = check_comm();
+    if comreq == False:
       init_comm()
-    if check_comm():
+    if comreq:
       req_ver()
       if hard != 0 and soft != 0:
         file_attach()
+        #req_arch()
+        #serialcmd = input("ENter pages: ").split('-')
+        # batch_req(int(serialcmd[0]), int(serialcmd[1]))
+        batch_req(1, 1000)
       else:
         logging.warn("<!> Cannot attach JSON file!")
-      serialcmd = input("ENter page: ")
-      x = "IF {}".format(serialcmd)
-      ser.write(x.encode())
-      time.sleep(1)
-      print(ser.in_waiting)
-      s = ser.read(7) #bytes size here
-      x = "IF {}".format(serialcmd)
-      print(x)
-      ser.write(x.encode())
-      s = ser.read(7) #bytes size here
-      s = ser.read_until(b'\x20') #verify start packet
-      print(s)
-      if s.decode() == "IF ":
-        s = ser.read_until(b'\x20') #bytes size heree
-        ssz = int(s.decode('utf-8')) #convert to right data type
-        print("size = {}".format(ssz)) #report this to console
-        s = ser.read(ssz) # read data stream from serial counted by received size
-        print(s.hex())
   except serial.serialutil.SerialException as err:
     logging.critical("<!> Com port not found! Check connection!")
     sys.exit()
+
+def req_arch():
+  serialcmd = input("ENter page: ")
+  x = "IF {}".format(serialcmd)
+  print(x)
+  ser.write(x.encode())
+  #s = ser.read(7) #bytes size here
+  s = ser.read_until(b'\x20') #verify start packet
+  if s.decode() == "IF ":
+     s = ser.read_until(b'\x20') #bytes size heree
+     ssz = int(s.decode('utf-8')) #convert to right data type
+     print("size = {}".format(ssz)) #report this to console
+     s = ser.read(ssz) # read data stream from serial counted by received size
+     print(s.hex())
+     parser(s)
   
   
-def parser():
+def parser(dataz, cls):
+  #print("<i> Parser")
   #parse data with JSON loaded schema
   pointerjson = 0
   datax = [];
   datas = [];
   bytebuff = bytearray()
   for n in range(22):
-    datax.append(s[pointerjson:][:datasizes[n]].hex())
+    datax.append(dataz[pointerjson:][:datasizes[n]].hex())
     pointerjson = pointerjson + datasizes[n]
-  print("------UNCONVERTED DATA!----------")
-  for n in range(22): 
-    out = "dataname: {} data: {}".format(datanames[n], datax[n])
-    print(out)
+  #print("------UNCONVERTED DATA!----------")
+  #for n in range(22): 
+  #  out = "dataname: {} data: {}".format(datanames[n], datax[n])
+  #  print(out)
 
   #define some vars before read convert type and pre-handle data using data type
   for n in range(22):
@@ -193,19 +200,52 @@ def parser():
         print("data from big")
         if str(buff)[0] != magic:
           print("data from little")
-          #buff = int.from_bytes(bytebuff, "big", signed=False)
-          sys.exit()
+          buff = int.from_bytes(bytebuff, "little", signed=False)
 #############################
-  ser.close()
-  print("-----------PARSED DATA!------------")
-  for n in range(22): 
-    out = "dataname: {} data: {}".format(datanames[n], datax[n])
-    print(out)
+  #print("-----------PARSED DATA!------------")
+  #for n in range(22): 
+  #  out = "dataname: {} data: {}".format(datanames[n], datax[n])
+    #print(out)
     #print(datetime.utcfromtimestamp(datax[5]).strftime('%Y-%m-%d %H:%M:%S'))
+  #datout = "{}".format(datax)
+  #print(datax)
+  file_dump(datax, cls)
+    
 
+def file_dump(datain, cls):
+  with open('datafile.log', 'a') as f:
+    #print('{}'.format(datain), file=f)
+    f.write("{}\n".format(datain))
+    if cls == True: f.close()
+
+def batch_req(start, end):
+  print("<i> Batch request")
+  cls = False
+  end = end + 1
+  for n in range(start, end):
+    #print(n)
+    x = "IF {}".format(n)
+    ser.write(x.encode())
+    s = ser.read_until(b'\x20') #verify start packet
+    if s.decode() == "IF ":
+      s = ser.read_until(b'\x20') #bytes size heree
+      ssz = int(s.decode('utf-8')) #convert to right data type
+      # print("size = {}".format(ssz)) #report this to console
+      s = ser.read(ssz) # read data stream from serial counted by received size
+      #print(s.hex())
+      if n == cls: cls = True
+      parser(s, cls)
+
+start_time = time.time()
+global datasizes
+datasizes = [];
+global datanames
+datanames = [];
+global datatypes
+datatypes = [];
 serialconn()
-
-
+ser.close()
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
---
+++ upd.py
@@ -0,0 +1,8 @@
+#update script
+import subprocess
+
+def get_git_revision_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+
+def get_git_revision_short_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
