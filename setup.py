25fa7f1832b87d3eb60c2dbc2fa7f949ae732341
UserD <theuserdead@gmail.com>
2022-11-28 14:55:34 +0300

    anotherone

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Gallileo_reporter',
+    version='0.1dev',
+    packages=['galileo_reporter',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
