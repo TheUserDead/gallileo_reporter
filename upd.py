7cd533e1a5d7036d14d19f30e1fcb17f277d1f3f
UserD <theuserdead@gmail.com>
2022-11-28 15:41:34 +0300

    test update

--- upd.py
+++ upd.py
@@ -9,4 +9,6 @@ def get_git_revision_short_hash() -> str:
 
 print(get_git_revision_hash())
 print(get_git_revision_short_hash())
-#tessssssst
+
+ rm -rf /home/gallileo_reporter/ && git clone https://github.com/TheUserDead/gallileo_reporter.git
+#mealhandsman
